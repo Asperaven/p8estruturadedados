ba88ed9ca92e87aabd8044d013016f7eb5d4a369
Erick Carneiro <102703036+Asperaven@users.noreply.github.com>
2023-10-16 18:08:20 -0300

    Atividade sobre estrutura de dados: listas.

---
+++ Fila.py
@@ -0,0 +1,34 @@
+from pacotes.ListaEncadeada import ListaEncadeada
+
+
+class Fila:
+    def __init__(self):
+        self.queue = ListaEncadeada()
+
+    def enfileirar(self, item):
+        self.queue.adicionar(item)
+
+    def desenfileirar(self):
+        if self.queue.inicioLista:
+            item = self.queue.inicioLista
+            self.queue.inicioLista = item.prox
+            return item.nome
+        else:
+            return None
+
+    def mostrar(self):
+        self.queue.exibir()
+
+
+# Examplo
+"""minha_fila = Fila()
+minha_fila.enfileirar("Item 1")
+minha_fila.enfileirar("Item 2")
+minha_fila.enfileirar("Item 3")"""
+
+print("Fila:")
+minha_fila.mostrar()  # Resultado: Item 1, Item 2, Item 3
+
+print("Item desinfileirado:", minha_fila.desenfileirar())  # Output: Item 1
+print("Fila após desenfileiramento:")
+minha_fila.mostrar()  # Resultado: Item 2, Item 3
---
+++ Pilha.py
@@ -0,0 +1,34 @@
+from pacotes.ListaEncadeada import ListaEncadeada
+
+
+class Pilha:
+    def __init__(self):
+        self.stack = ListaEncadeada()
+
+    def empilhar(self, item):
+        self.stack.adicionar(item)
+
+    def desempilhar(self):
+        if self.stack.inicioLista:
+            item = self.stack.inicioLista
+            self.stack.inicioLista = item.prox
+            return item.nome
+        else:
+            return None
+
+    def mostrar(self):
+        self.stack.exibir()
+
+
+# Examplo
+"""minha_pilha = Pilha()
+minha_pilha.empilhar("Item 1")
+minha_pilha.empilhar("Item 2")
+minha_pilha.empilhar("Item 3")"""
+
+print("Pilha:")
+minha_pilha.mostrar()  # Resultado: Item 1, Item 2, Item 3
+
+print("Item desempilhado:", minha_pilha.desempilhar())  # Resultado: Item 3
+print("Pilha depois do desempilhamento:")
+minha_pilha.mostrar()  # Output: Item 1, Item 2
